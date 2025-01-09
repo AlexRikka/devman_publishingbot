30ab001b4d25117aaa18f46ccd50c83f547c9259
AlexRikka <rikka.gold@mail.ru>
2025-01-09 23:11:37 +0400

    initial commit

---
+++ bot.py
@@ -0,0 +1,43 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        'Здравствуйте!'
+    )
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    load_dotenv()
+    updater = Updater(os.environ['TG_BOT_TOKEN'])
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(
+        Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
