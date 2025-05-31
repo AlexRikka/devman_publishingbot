4d7b628881c246829bcc0f0d38953affb1b6d03b
AlexRikka <rikka.gold@mail.ru>
2025-06-01 00:44:02 +0400

    add DialogFlow learning script

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
+import google.cloud.dialogflow_v2 as dialogflow
 import logging
 import os
 
 from dotenv import load_dotenv
+from google.api_core.exceptions import InvalidArgument
+from google.oauth2 import service_account
+
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-
+from functools import partial
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,26 +18,67 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def start(update: Update, context: CallbackContext) -> None:
+class DialogflowSession():
+    def __init__(self, dialogflow_project_id,
+                 dialogflow_language_code,
+                 session_id,
+                 session_client,
+                 session):
+        self.dialogflow_project_id = dialogflow_project_id
+        self.dialogflow_language_code = dialogflow_language_code
+        self.session_id = session_id
+        self.session_client = session_client
+        self.session = session
+
+
+def start(update, context):
     update.message.reply_text(
         'Здравствуйте!'
     )
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def echo(update, context, connection):
+    text_input_tg = update.message.text
+    text_input = dialogflow.types.TextInput(
+        text=text_input_tg, language_code=connection.dialogflow_language_code)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+
+    try:
+        response = connection.session_client.detect_intent(
+            session=connection.session, query_input=query_input)
+    except InvalidArgument:
+        raise
+
+    update.message.reply_text(response.query_result.fulfillment_text)
 
 
 def main() -> None:
-    load_dotenv()
-    updater = Updater(os.environ['TG_BOT_TOKEN'])
+    load_dotenv(override=True)
+    print(dict(os.environ))
 
+    # dialogflow setup
+    DIALOGFLOW_PROJECT_ID = os.environ['DIALOGFLOW_PROJECT_ID']
+    DIALOGFLOW_LANGUAGE_CODE = os.environ['DIALOGFLOW_LANGUAGE_CODE']
+    SESSION_ID = os.environ['SESSION_ID']
+    credentials_file = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+    print(credentials_file)
+    credentials = service_account.Credentials.from_service_account_file(
+        credentials_file)
+    session_client = dialogflow.SessionsClient(credentials=credentials)
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+
+    dialogflow_session = DialogflowSession(DIALOGFLOW_PROJECT_ID,
+                                           DIALOGFLOW_LANGUAGE_CODE,
+                                           SESSION_ID,
+                                           session_client,
+                                           session)
+
+    # TG setup
+    updater = Updater(os.environ['TG_BOT_TOKEN'], use_context=True)
     dispatcher = updater.dispatcher
-
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(
-        Filters.text & ~Filters.command, echo))
+        Filters.text & ~Filters.command, partial(echo, connection=dialogflow_session)))
 
     updater.start_polling()
     updater.idle()
---
+++ create_intent.py
@@ -0,0 +1,50 @@
+import google.cloud.dialogflow_v2 as dialogflow
+import os
+import json
+from dotenv import load_dotenv
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(
+            text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+def main():
+    load_dotenv()
+    DIALOGFLOW_PROJECT_ID = os.environ['DIALOGFLOW_PROJECT_ID']
+
+    with open("questions.json", "r", encoding='utf-8') as my_file:
+        intents_json = my_file.read()
+
+    intents = json.loads(intents_json)
+    for intent in intents:
+        create_intent(DIALOGFLOW_PROJECT_ID,
+                      intent,
+                      intents[intent]['questions'],
+                      [intents[intent]['answer']])
+
+
+if __name__ == '__main__':
+    main()
