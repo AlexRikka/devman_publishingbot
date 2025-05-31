2368dcc9049de326acd5ceedb9c6502a0cd759d8
AlexRikka <rikka.gold@mail.ru>
2025-06-01 01:07:40 +0400

    make bot not answer if it didn't understand question

--- bot.py
+++ bot.py
@@ -40,7 +40,8 @@ def echo(update, context, connection):
     except InvalidArgument:
         raise
 
-    update.message.reply_text(response.query_result.fulfillment_text)
+    if not response.query_result.intent.is_fallback:
+        update.message.reply_text(response.query_result.fulfillment_text)
 
 
 def main() -> None:
